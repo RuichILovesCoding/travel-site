b4f2b27471a89d173b7911d6ae9263dacaa8eaa9
Ruichi Wu <wurc199733@gmail.com>
2020-03-01 10:49:54 +1100

    reveal on Scroll finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,13 @@
 import "../styles/styles.css";
 import Person from "./modules/Person";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 if (module.hot) {
   module.hot.accpet;
 }
 
-/* example codes*/
+/* example codes
 
 class Adult extends Person {
   payTaxes() {
@@ -18,8 +19,10 @@ let john = new Person("John Smith", "Red");
 john.greet();
 
 let jane = new Adult("Jane t", "Blue");
-jane.payTaxes();
+jane.payTaxes();*/
 
 /*begin build the website*/
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
