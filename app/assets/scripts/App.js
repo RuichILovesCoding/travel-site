e151345085bc8c36523cc296fa4630b41b6c8029
Ruichi Wu <wurc199733@gmail.com>
2020-03-01 14:10:02 +1100

    compelte the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -28,3 +28,20 @@ let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let stickyHeader = new StickyHeader();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach(el =>
+  el.addEventListener("click", e => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName:"modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There is a problem..."));
+    } else {
+      modal.openTheModal();
+    }
+  })
+);
